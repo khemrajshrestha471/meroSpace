aa080cedd8ae3b8d47eb0a34af5835536afabbdb
Khemraj Shrestha <khemrajshrestha471@gmail.com>
2024-10-18 14:40:26 +0545

    login-as-uploader UI completed with necessary input fields

--- src/app/login-as-uploader/page.tsx
+++ src/app/login-as-uploader/page.tsx
@@ -1,9 +1,139 @@
-import React from 'react'
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import Image from "next/image";
+
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+const FormSchema = z
+  .object({
+
+    email: z.string().email({ message: "Invalid email address." }),
+
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long." })
+      .regex(/[a-z]/, {
+        message: "Password must contain at least one lowercase letter.",
+      })
+      .regex(/[A-Z]/, {
+        message: "Password must contain at least one uppercase letter.",
+      })
+      .regex(/[0-9]/, { message: "Password must contain at least one number." })
+      .regex(/[\W_]/, {
+        message: "Password must contain at least one special character.",
+      }),
+  })
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
 
-export default page
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState: any) => !prevState);
+  };
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log("This is data:- ", data.email);
+    console.log("This is data:- ", data.password);
+  }
+
+  const handleClear = () => {
+    form.reset();
+  };
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="flex justify-center items-center">
+        <Image
+          src="/assets/gifs/login.gif"
+          width={650}
+          height={650}
+          alt="Nepal's Flag"
+        />
+      </div>
+      <div className="flex justify-center items-center">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-2/3 space-y-6"
+          >
+
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Your Registered Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <div className="relative">
+                    <FormControl>
+                      <Input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="Your Registered Password"
+                        {...field}
+                        className="pr-10"
+                      />
+                    </FormControl>
+                    <div
+                      className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
+                      onClick={togglePasswordVisibility}
+                    >
+                      {showPassword ? <IoMdEyeOff /> : <IoMdEye />}
+                    </div>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex space-x-4">
+              <Button type="submit">Login</Button>
+              <Button type="button" variant="destructive" onClick={handleClear}>
+                Clear
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default page;
+
+
+
+// if password critria did not match, then don't hit databse, directly show incorrect credentials
